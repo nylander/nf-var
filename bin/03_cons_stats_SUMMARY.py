d286a57be9f038bfa6e9ab45df14648ff59818e7
nylander <Johan.Nylander@nrm.se>
2025-02-03 16:42:24 +0100

    Run pylint

--- bin/03_cons_stats_SUMMARY.py
+++ bin/03_cons_stats_SUMMARY.py
@@ -1,18 +1,31 @@
 #!/usr/bin/env python3
 
+"""
+Helper script 03_cons_stats_SUMMARY.py for nf-var
+"""
+
 #########################
 ## Required modules
 #########################
-try:
-	import os
-	import sys
-	import argparse
-	import pandas as pd
-	import matplotlib
-	matplotlib.use('Agg')
-	import matplotlib.pyplot as plt
-except ImportError:
-	sys.exit("One of the required modules can't be found...")
+
+import os
+import argparse
+import seaborn as sns
+import pandas as pd
+import matplotlib
+import matplotlib.pyplot as plt
+matplotlib.use('Agg')
+
+#try:
+#    import os
+#    import sys
+#    import argparse
+#    import pandas as pd
+#    import matplotlib
+#    matplotlib.use('Agg')
+#    import matplotlib.pyplot as plt
+#except ImportError:
+#    sys.exit("One of the required modules can't be found...")
 
 
 #########################
@@ -21,8 +34,15 @@ except ImportError:
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument("-c", "--cons_stats_fn_list", help="List of consensus stats files --> paths", action = "store", nargs='+', default=[], required=True)
-parser.add_argument("-o", "--out_dir", help="Directory to store the overview file, if unspecified assumes current work dir", default='current', action = "store", required=False)
+parser.add_argument(
+        "-c", "--cons_stats_fn_list",
+        help="List of consensus stats files --> paths",
+        action="store", nargs='+', default=[], required=True)
+
+parser.add_argument(
+        "-o", "--out_dir",
+        help="Directory to store the overview file, if unspecified assumes current work dir",
+        action="store", default='current', required=False)
 
 args = parser.parse_args()
 
@@ -30,9 +50,9 @@ args = parser.parse_args()
 consensus_stats_list = args.cons_stats_fn_list
 output_path = args.out_dir
 if output_path != 'current':
-	pass
+    pass
 else:
-	output_path = os.getcwd()
+    output_path = os.getcwd()
 
 
 #########################
@@ -43,24 +63,24 @@ df = pd.DataFrame(columns=['Indiv', 'Missing_data', 'Missing_data_PERCENTAGE'])
 
 ## Loop over consensus stats files, parse data and store
 for indiv in consensus_stats_list:
-	indiv_id = os.path.basename(indiv).split('_')[0]
-	indiv_df = pd.read_csv(indiv, sep='\t', index_col=0)
-	indiv_sum_df = pd.DataFrame([[indiv_id, indiv_df.loc['Total']['Missing_data'], indiv_df.loc['Total']['Missing_data_PERCENTAGE']]], columns=['Indiv', 'Missing_data', 'Missing_data_PERCENTAGE'])
-	df = pd.concat([df, indiv_sum_df], ignore_index=True)
+    indiv_id = os.path.basename(indiv).split('_')[0]
+    indiv_df = pd.read_csv(indiv, sep='\t', index_col=0)
+    indiv_sum_df = pd.DataFrame([[indiv_id, indiv_df.loc['Total']['Missing_data'],
+                                  indiv_df.loc['Total']['Missing_data_PERCENTAGE']]],
+                                columns=['Indiv', 'Missing_data', 'Missing_data_PERCENTAGE'])
+    df = pd.concat([df, indiv_sum_df], ignore_index=True)
 
 ## Write out file
 tsv_fn = os.path.join(output_path, ('all_indivs_missing_data.tsv'))
 df.to_csv(tsv_fn, sep='\t', index=False)
 
 # ## Now let's plot as well a basic graph to highlight missing data per indiv
-# fig, axs = plt.subplots(figsize=(4, 12)) 
+# fig, axs = plt.subplots(figsize=(4, 12))
 # df.plot(x="Indiv", y="Missing_data_PERCENTAGE", kind="barh", fontsize=4, legend=False, ax=axs)
 # axs.set_xlim(0, 100)
 # axs.set_title("Ratio missing data per individual")
 # fig.savefig(os.path.join(output_path, "all_indivs_missing_data.pdf"), dpi=300)
 
-import seaborn as sns
-import matplotlib.pyplot as plt
 
 ## - Specify plot, if need be multiple tiers
 sns.set_theme(style="whitegrid")
@@ -81,11 +101,11 @@ titles = ["Percentage missing data"]
 
 for ax, title in zip(g.axes.flat, titles):
 
-	ax.xaxis.grid(False)
-	ax.yaxis.grid(True)
+    ax.xaxis.grid(False)
+    ax.yaxis.grid(True)
 
-	ax.set_ylabel('Individuals', fontweight="bold", fontsize=18, labelpad=20)
-	ax.set_xlabel('Missing data percentage', fontweight="bold", fontsize=18, labelpad=20)
+    ax.set_ylabel('Individuals', fontweight="bold", fontsize=18, labelpad=20)
+    ax.set_xlabel('Missing data percentage', fontweight="bold", fontsize=18, labelpad=20)
 
 sns.despine(left=True, bottom=True)
 

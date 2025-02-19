1cc268086c0f54e14c1926908a78d869910f8c6e
nylander <Johan.Nylander@nrm.se>
2025-02-19 15:38:54 +0100

    change settingsfrom wrok dir in dardel slurmfile

--- run_nf-var_DARDEL.sh
+++ run_nf-var_DARDEL.sh
@@ -6,20 +6,22 @@
 #SBATCH --partition=shared
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
-#SBATCH --time=6-12:00:0
+#SBATCH --time=7-00:00:0
 
 ml PDC/23.12
 ml java/17.0.4
-ml singularity
 ml python/3.12.3
+#ml singularity
 
 export NXF_OPTS='-Xms1g -Xmx4g'
 export NXF_CONDA_CACHEDIR=/cfs/klemming/projects/supr/nrmdnalab_storage/src/NXF_CONDA_CACHEDIR
-export NXF_SINGULARITY_CACHEDIR=/cfs/klemming/projects/supr/nrmdnalab_storage/src/NXF_SINGULARITY_CACHEDIR
+#export NXF_SINGULARITY_CACHEDIR=/cfs/klemming/projects/supr/nrmdnalab_storage/src/NXF_SINGULARITY_CACHEDIR
 
+cd /cfs/klemming/projects/supr/nrmdnalab_storage/src/nf-var
 nextflow run \
-    /cfs/klemming/projects/supr/nrmdnalab_storage/src/nf-var/main.nf \
-    -name run_nf_var  \
+    ./main.nf \
+    -with-conda \
+    -name run_nf_var \
+    -work-dir $SNIC_TMP/run_nf_var/work \
     -profile pdc_kth \
     --project naiss2024-22-1518
-
